955b6b1398a73463d6c21221c313df48ef767df5
ctscofield <ctscofield@gmail.com>
2021-07-27 14:23:38 -0600

    added classesMiddleware file to middleware folder

---
+++ api/middleware/classesMiddleware.js
@@ -0,0 +1,44 @@
+const Class = require("../classes/classesModel");
+
+async function validateUserId(req, res, next) {
+  try {
+    const user = await Class.getById(req.params.id);
+    if (!user) {
+      next({status: 404, message: "user not found"});
+    } else {
+      req.user = user;
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "problem finding user"
+    })
+  }
+
+};
+
+function validateUser(req, res, next) {
+  const { name } = req.body;
+  if (!name || name.trim()) {
+    res.status(400).json({message: "missing required name field"})
+  } else {
+    req.name = name.trim();
+    next();
+  }
+};
+
+function validatePost(req, res, next) {
+  const { text } = req.body;
+  if (!text || text.trim()) {
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    req.text = text.trim();
+    next();
+  }
+};
+
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+};
