288f37e8f1aabf72a285377ca211dcce4939905c
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-27 13:02:43 -0500

    router/login - login end point built and working

--- api/authentication/authentication-router.js
+++ api/authentication/authentication-router.js
@@ -12,44 +12,37 @@ const {
 router.post('/register', (req, res, next) => {
     // add middleware - 1 - check payload,  2- unique username
 
-    console.log("POST - REGISTER end point connected")
-    next()
-
-    // const { username, password, role_name } = req.body
-
-    // const hash = bcrypt.hashSync(password, 8)
-
-    // Users.add({ username, password: hash, role_name })
-    //     .then(newUser => {
-    //         res.status(200).json(newUser) // expand on .json message?
-    //     })
-    //     .catch(next) 
-})
-
-router.post('/login', (req, res, next) => {
-    // middleware - 1 - check payload,  2- check login payload - for matching credentials
-
-    console.log("POST - LOGIN end point connected")
+    // console.log("POST - REGISTER end point connected")
     // next()
 
-    // async await?
-    // try / catch vs. .then / .catch?
+    const { username, password, role_name } = req.body
 
-    // const { username, password } = req.body
-  
-    // Users.findByUsername(username)
-    // .then(([user]) => {
-    // if (user && bcrypt.compareSync(password, user.password)) {
-    //     const token = tokenBuilder(user)
-    //     res.status(200).json({
-    //     message: `welcome, ${username}`,
-    //     token
-    //     })
-    //     } else {
-    //     next({ status:401, message: 'invalid credentials' })
-    //     }
-    // })
-    // .catch(next)
+    const hash = bcrypt.hashSync(password, 8)
+
+    Users.add({ username, password: hash, role_name })
+        .then(newUser => {
+            res.status(200).json(newUser) // expand on .json message?
+        })
+        .catch(next) 
+})
+
+router.post('/login', checkPayload, checkLoginPayload, async (req, res, next) => {
+     try {
+        const { username, password } = req.body
+        const user = await Users.findByUsername(username)
+
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = tokenBuilder(user)
+            res.status(200).json({
+                message: `welcome, ${username}`,
+                token
+            })
+            } else {
+                next({ status:401, message: 'invalid credentials' })
+            }
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router;
