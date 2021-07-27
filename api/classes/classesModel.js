2a83fe50521f75d1779719cad7c26bdc69eb8335
ctscofield <ctscofield@gmail.com>
2021-07-27 11:06:22 -0600

    addition of classes from cory branch

---
+++ api/classes/classesModel.js
@@ -0,0 +1,48 @@
+// COPY AND PASTED FROM NODE-API3-PROJECT WEEK 1 DAY 3
+
+const db = require('../data/db-config');
+
+
+
+function get() {
+  return db('users');
+}
+
+function getById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+
+function insert(user) {
+  return db('users')
+    .insert(user)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+function update(id, changes) {
+  return db('users')
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return getById(id);
+    });
+}
+
+function remove(id) {
+  return db('users')
+    .where('id', id)
+    .del();
+}
+
+
+module.exports = {
+  get,
+  getById,
+  insert,
+  update,
+  remove,
+};
