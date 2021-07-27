14b2359e1c81f5c319519e87dea925e56d2bd9a3
ctscofield <ctscofield@gmail.com>
2021-07-27 15:51:09 -0600

    middleware

--- api/middleware/classesMiddleware.js
+++ api/middleware/classesMiddleware.js
@@ -35,18 +35,8 @@ function validateClassPayload(req, res, next) {
   }
 }
 
-function validatePost(req, res, next) {
-  const { text } = req.body;
-  if (!text || text.trim()) {
-    res.status(400).json({message: "missing required text field"})
-  } else {
-    req.text = text.trim();
-    next();
-  }
-};
 
 module.exports = {
   validateUserId,
   validateClassPayload,
-  validatePost,
 };
